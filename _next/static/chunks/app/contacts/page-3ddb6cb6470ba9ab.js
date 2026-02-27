f55354ac255dce089694fc18ec4512a83dfe2409
muzychenkooo <muzychenkooo@users.noreply.github.com>
2026-02-27 22:00:02 +0000

    deploy: 486eeb524dfdc50fb6f8fbf5ac47256e70ee931d

---
+++ _next/static/chunks/app/contacts/page-3ddb6cb6470ba9ab.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[613],{7569:function(e,n,s){Promise.resolve().then(s.bind(s,7674)),Promise.resolve().then(s.t.bind(s,2972,23))}},function(e){e.O(0,[972,851,626,674,971,117,744],function(){return e(e.s=7569)}),_N_E=e.O()}]);
